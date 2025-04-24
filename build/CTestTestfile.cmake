616592b08ed03fe4163af91707dcd573575fa7dd
Adnan Mazharuddin Shaikh <adnanmazharuddinshaikh@gmail.com>
2025-04-23 17:03:55 -0700

    added polynomial regression

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/sanchit/Documents/ADS/Projects/ALEX_CODE/Poly_ALEX
+# Build directory: /Users/sanchit/Documents/ADS/Projects/ALEX_CODE/Poly_ALEX/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_alex "test_alex")
+set_tests_properties(test_alex PROPERTIES  _BACKTRACE_TRIPLES "/Users/sanchit/Documents/ADS/Projects/ALEX_CODE/Poly_ALEX/CMakeLists.txt;82;add_test;/Users/sanchit/Documents/ADS/Projects/ALEX_CODE/Poly_ALEX/CMakeLists.txt;0;")
